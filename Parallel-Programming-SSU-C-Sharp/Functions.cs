97cbb6893d4354b10c8348d7a7150dafe1061ab5
Kirill Lukashin <lukashinkv@gmail.com>
2020-11-19 23:22:29 +0400

    are u kidding

---
+++ Parallel-Programming-SSU-C-Sharp/Functions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Parallel_Programming_SSU_C_Sharp
+{
+    class Functions
+    {
+        public double CalcRow(double x)
+        {
+            x = 100 * Math.Cos(x);
+            double y = 0;
+            for (int k = 1; k < Math.Max(20.0, 20 * Math.Abs(x)); k++) {
+                for (int j = 1; j < Math.Max(20.0, 20 * Math.Abs(x)); j++) {
+                    y += (x * x * (k + j)) / (x * x + k * k * k + j * j * j) * Math.Cos(k * x) * Math.Sin(j * x);
+                }
+            }
+
+            return y;
+        }
+    }
+
+    class ParallelForDemo
+    {
+        static void Main1(string[] args)
+        {
+            const int N = 100;
+            
+            Functions functions = new Functions();
+            double[] array = new double [N];
+            
+            Int64 ticks = (DateTime.Now).Ticks;
+            for (int k = 0; k < N; k++)
+            {
+                array[k] = functions.CalcRow(k);
+            }
+            ticks = (DateTime.Now).Ticks - ticks;
+            TimeSpan time = new TimeSpan(ticks);
+            Console.WriteLine("Sequential algorithm: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.For(0, N, k => { array[k] = functions.CalcRow(k); });
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("Parallel algorithm " + (time.TotalSeconds).ToString() + " seconds");
+        }
+    }
+}
--- Parallel-Programming-SSU-C-Sharp/ParallelForEach.cs
+++ Parallel-Programming-SSU-C-Sharp/ParallelForEach.cs
@@ -1,61 +1,206 @@
 ﻿//ParallelForEach.cs
 
 using System;
+using System.Collections.Generic;
 
 namespace Parallel_Programming_SSU_C_Sharp
-{
-    class ParallelForEach
-    {
-        public double Func(double x)
-        {
-            int N = Math.Max(20, (int) Math.Floor(20 * Math.Abs(x)));
-            double S = 0;
-            double x2 = x * x;
-            for (int k = 1; k <= N; k++)
-            for (int j = 1; j <= N; j++)
-                S += x2 * Math.Sin((k + j) * x) / (x2 + k * k + j * j);
-            return S;
-        }
-    };
-
+{ 
     class ParallelForEachDemo
     {
         static void Main1(string[] args)
         {
-            const int N = 200;
-            ParallelForEach fobj = new ParallelForEach();
-            double[] X = new double[N + 1];
+            const int N = 100;
+            Functions functions = new Functions();
+            double[] array = new double[N + 1];
             for (int k = 0; k <= N; k++)
             {
-                X[k] = 100 * Math.Cos(1.0 * k);
+                array[k] = 100 * Math.Cos(1.0 * k);
             }
 
             double S = 0;
-            Int64 Tms = (DateTime.Now).Ticks;
-            foreach (double x in X) S += fobj.Func(x);
-            Tms = (DateTime.Now).Ticks - Tms;
-            TimeSpan Tmss = new TimeSpan(Tms);
+            Int64 ticks = (DateTime.Now).Ticks;
+            foreach (double x in array) S += functions.CalcRow(x);
+            ticks = (DateTime.Now).Ticks - ticks;
+            TimeSpan time = new TimeSpan(ticks);
             Console.WriteLine("S=" + S.ToString());
-            Console.WriteLine("Время выполнения последовательной операции foreach " + (Tmss.TotalSeconds).ToString() +
-                              " c");
+            Console.WriteLine("Sequential algorithm: " + (time.TotalSeconds).ToString() + " seconds");
 
-            Object obj = new Object();
             S = 0;
-            Tms = (DateTime.Now).Ticks;
-            System.Threading.Tasks.Parallel.ForEach(X,
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(array,
                 x =>
                 {
-                    double Tmp = fobj.Func(x);
-                    lock (obj)
-                    {
-                        S += Tmp;
-                    }
+                    S += functions.CalcRow(x);
                 }
             );
-            Tms = (DateTime.Now).Ticks - Tms;
-            Tmss = new TimeSpan(Tms);
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
             Console.WriteLine("S=" + S.ToString());
-            Console.WriteLine("Время выполнения параллельного метода ForEach " + (Tmss.TotalSeconds).ToString() + " c");
+            Console.WriteLine("Parallel algorithm: " + (time.TotalSeconds).ToString() + " seconds");
+        }
+
+        static void Main2(string[] args)
+        {
+            const int N = 100;
+            Functions functions = new Functions();
+            List<double> list = new List<double>();
+            Stack<double> stack = new Stack<double>();
+            Queue<double> queue = new Queue<double>();
+            for (int k = 0; k <= N; k++)
+            {
+                double tmp = 100 * Math.Cos(1.0 * k);
+                list.Add(tmp);
+                stack.Push(tmp);
+                queue.Enqueue(tmp);
+            }
+
+            double S = 0;
+            Int64 ticks = (DateTime.Now).Ticks;
+            foreach (double x in list) S += functions.CalcRow(x);
+            ticks = (DateTime.Now).Ticks - ticks;
+            TimeSpan time = new TimeSpan(ticks);
+            Console.WriteLine("S=" + S.ToString());
+            Console.WriteLine("Sequential algorithm for list: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            S = 0;
+            ticks = (DateTime.Now).Ticks;
+            foreach (double x in stack) S += functions.CalcRow(x);
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("S=" + S.ToString());
+            Console.WriteLine("Sequential algorithm for stack: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            S = 0;
+            ticks = (DateTime.Now).Ticks;
+            foreach (double x in queue) S += functions.CalcRow(x);
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("S=" + S.ToString());
+            Console.WriteLine("Sequential algorithm for queue: " + (time.TotalSeconds).ToString() + " seconds");
+
+            S = 0;
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(list,
+                x =>
+                {
+                    S += functions.CalcRow(x);
+                }
+            );
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("S=" + S.ToString());
+            Console.WriteLine("Parallel algorithm for list: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            S = 0;
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(stack,
+                x =>
+                {
+                    S += functions.CalcRow(x);
+                }
+            );
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("S=" + S.ToString());
+            Console.WriteLine("Parallel algorithm for stack: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            S = 0;
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(queue,
+                x =>
+                {
+                    S += functions.CalcRow(x);
+                }
+            );
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("S=" + S.ToString());
+            Console.WriteLine("Parallel algorithm for queue: " + (time.TotalSeconds).ToString() + " seconds");
+        }
+    
+    
+    static void Main(string[] args)
+        {
+            const int N = 100;
+            Functions functions = new Functions();
+            List<double> list = new List<double>();
+            Stack<double> stack = new Stack<double>();
+            Queue<double> queue = new Queue<double>();
+            
+            List<double> listY = new List<double>();
+            Stack<double> stackY = new Stack<double>();
+            Queue<double> queueY = new Queue<double>();
+            for (int k = 0; k <= N; k++)
+            {
+                double tmp = 100 * Math.Cos(1.0 * k);
+                list.Add(tmp);
+                stack.Push(tmp);
+                queue.Enqueue(tmp);
+            }
+
+            Int64 ticks = (DateTime.Now).Ticks;
+            foreach (double x in list)
+            {
+                listY.Add(functions.CalcRow(x));
+            }
+            ticks = (DateTime.Now).Ticks - ticks;
+            TimeSpan time = new TimeSpan(ticks);
+            Console.WriteLine("Sequential algorithm for list: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            ticks = (DateTime.Now).Ticks;
+            foreach (double x in stack)
+            {
+                stackY.Push(functions.CalcRow(x));
+            }
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("Sequential algorithm for stack: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            ticks = (DateTime.Now).Ticks;
+            foreach (double x in queue)
+            {
+                queueY.Enqueue(functions.CalcRow(x));
+            }
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("Sequential algorithm for queue: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            listY = new List<double>();
+            stackY = new Stack<double>();
+            queueY = new Queue<double>();
+
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(list,
+                x =>
+                {
+                     listY.Add(functions.CalcRow(x));
+                }
+            );
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("Parallel algorithm for list: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(stack,
+                x =>
+                {
+                    stackY.Push(functions.CalcRow(x));
+                }
+            );
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("Parallel algorithm for stack: " + (time.TotalSeconds).ToString() + " seconds");
+            
+            ticks = (DateTime.Now).Ticks;
+            System.Threading.Tasks.Parallel.ForEach(queue,
+                x =>
+                {
+                    queueY.Enqueue(functions.CalcRow(x));
+                }
+            );
+            ticks = (DateTime.Now).Ticks - ticks;
+            time = new TimeSpan(ticks);
+            Console.WriteLine("Parallel algorithm for queue: " + (time.TotalSeconds).ToString() + " seconds");
         }
     }
 }
